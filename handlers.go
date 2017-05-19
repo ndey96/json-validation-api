94d1e5ad626f0d79130082c675f73227a48aaf45
Nolan Dey <ndey96@gmail.com>
2017-05-19 01:52:38 -0400

    Add validate endpoint

--- handlers.go
+++ handlers.go
@@ -7,22 +7,15 @@ import (
     "io"
     "io/ioutil"
     "github.com/gorilla/mux"
+    "github.com/xeipuuv/gojsonschema"
 )
 
 func Index(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "Welcome!")
+    fmt.Fprintln(w, "Server is alive!")
 }
 
-func SchemaIndex(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-    w.WriteHeader(http.StatusOK)
-    if err := json.NewEncoder(w).Encode(schemas); err != nil {
-        panic(err)
-    }
-}
-
-func SchemaShow(w http.ResponseWriter, r *http.Request) {
-    action := "DownloadSchema"
+func DownloadSchema(w http.ResponseWriter, r *http.Request) {
+    action := "downloadSchema"
     vars := mux.Vars(r)
     id := vars["schemaId"]
     s := RepoFindSchema(vars["schemaId"])
@@ -42,8 +35,8 @@ func SchemaShow(w http.ResponseWriter, r *http.Request) {
     }
 }
 
-func SchemaCreate(w http.ResponseWriter, r *http.Request) {
-  action := "UploadSchema"
+func UploadSchema(w http.ResponseWriter, r *http.Request) {
+  action := "uploadSchema"
   vars := mux.Vars(r)
   id := vars["schemaId"]
   schema := Schema{Id: id}
@@ -71,5 +64,54 @@ func SchemaCreate(w http.ResponseWriter, r *http.Request) {
   res := Response{Action: action, Status: "success", Id: id}
   if err := json.NewEncoder(w).Encode(res); err != nil {
     panic(err)
+  }
+}
+
+func ValidateDocument(w http.ResponseWriter, r *http.Request) {
+  action := "validateDocument"
+  vars := mux.Vars(r)
+  schemaId := vars["schemaId"]
+
+  document, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
+  if err != nil {
+    panic(err)
+  }
+  if err := r.Body.Close(); err != nil {
+    panic(err)
+  }
+  var js map[string]interface{}
+  if err := json.Unmarshal(document, &js); err != nil {
+    w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+    w.WriteHeader(http.StatusUnprocessableEntity)
+    res := ResponseWithMessage{Action: action, Status: "error", Id: schemaId, Message: "Document is not valid JSON"}
+    if err := json.NewEncoder(w).Encode(res); err != nil {
+      panic(err)
+    }
+    return
+  }
+  schema := RepoFindSchema(vars["schemaId"])
+  schemaLoader := gojsonschema.NewStringLoader(string(document[:]))
+  documentLoader := gojsonschema.NewStringLoader(string(schema.Schema[:]))
+  result, err := gojsonschema.Validate(schemaLoader, documentLoader)
+  if err != nil {
+    panic(err)
+  }
+
+  if result.Valid() {
+    w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+    w.WriteHeader(http.StatusOK)
+    res := Response{Action: action, Status: "success", Id: schemaId}
+    if err := json.NewEncoder(w).Encode(res); err != nil {
+      panic(err)
+    }
+    return
+  } else {
+    w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+    w.WriteHeader(http.StatusUnprocessableEntity)
+    res := ResponseWithMessage{Action: action, Status: "error", Id: schemaId, Message: "Document does not conform to schema"}
+    if err := json.NewEncoder(w).Encode(res); err != nil {
+      panic(err)
+    }
+    return
   }
 }
--- routes.go
+++ routes.go
@@ -3,37 +3,37 @@ package main
 import "net/http"
 
 type Route struct {
-    Name        string
-    Method      string
-    Pattern     string
-    HandlerFunc http.HandlerFunc
+  Name        string
+  Method      string
+  Pattern     string
+  HandlerFunc http.HandlerFunc
 }
 
 type Routes []Route
 
 var routes = Routes{
-    Route{
-        "Index",
-        "GET",
-        "/",
-        Index,
-    },
-    Route{
-        "SchemaIndex",
-        "GET",
-        "/schemas",
-        SchemaIndex,
-    },
-    Route{
-        "SchemaShow",
-        "GET",
-        "/schemas/{schemaId}",
-        SchemaShow,
-    },
-        Route{
-        "SchemaCreate",
-        "POST",
-        "/schemas/{schemaId}",
-        SchemaCreate,
-    },
+  Route{
+    "Index",
+    "GET",
+    "/",
+    Index,
+  },
+  Route{
+    "DownloadSchema",
+    "GET",
+    "/schema/{schemaId}",
+    DownloadSchema,
+  },
+  Route{
+    "UploadSchema",
+    "POST",
+    "/schema/{schemaId}",
+    UploadSchema,
+  },
+  Route{
+    "ValidateDocument",
+    "POST",
+    "/validate/{schemaId}",
+    ValidateDocument,
+  },
 }
