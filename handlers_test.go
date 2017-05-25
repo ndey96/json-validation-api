673df47be684b95da23b5845bd267736a6a6b32e
Nolan Dey <ndey96@gmail.com>
2017-05-25 12:06:48 -0400

    Add validateDocument test

--- handlers_test.go
+++ handlers_test.go
@@ -41,7 +41,7 @@ func TestUploadSchema(t *testing.T) {
   handler := http.HandlerFunc(UploadSchema)
   m := mux.NewRouter()
   m.HandleFunc("/schema/{schemaId}", handler)
-  rawSchema, err := ioutil.ReadFile("./testJSON/test-schema.json")
+  rawSchema, err := ioutil.ReadFile("./json/test-schema.json")
   FailIf(err, t)
 
   // upload a schema
@@ -63,7 +63,7 @@ func TestUploadSchema(t *testing.T) {
   ExpectValue(expectedBody, strings.Trim(w.Body.String(), "\n"), "body", t)
 
   // upload an invalid schema
-  rawSchema, err = ioutil.ReadFile("./testJSON/invalid-schema.json")
+  rawSchema, err = ioutil.ReadFile("./json/invalid-schema.json")
   FailIf(err, t)
   req, err = http.NewRequest("POST", "/schema/test-schema", bytes.NewBuffer(rawSchema))
   FailIf(err, t)
@@ -71,5 +71,52 @@ func TestUploadSchema(t *testing.T) {
   m.ServeHTTP(w, req)
   ExpectValue(http.StatusUnprocessableEntity, w.Code, "status", t)
   expectedBody = `{"action":"uploadSchema","id":"test-schema","status":"error","message":"Invalid JSON provided"}`
+  ExpectValue(expectedBody, strings.Trim(w.Body.String(), "\n"), "body", t)
+}
+
+func TestValidateDocument(t *testing.T) {
+  // setup
+  db := OpenDBConn()
+  defer db.Close()
+  ResetTestSchemas(db, t)
+  m := mux.NewRouter()
+
+  // upload a schema
+  handler := http.HandlerFunc(UploadSchema)
+  m.HandleFunc("/schema/{schemaId}", handler)
+  rawSchema, err := ioutil.ReadFile("./json/test-schema.json")
+  FailIf(err, t)
+  req, err := http.NewRequest("POST", "/schema/test-schema", bytes.NewBuffer(rawSchema))
+  FailIf(err, t)
+  w := httptest.NewRecorder()
+  m.ServeHTTP(w, req)
+  ExpectValue(http.StatusCreated, w.Code, "status", t)
+  expectedBody := `{"action":"uploadSchema","id":"test-schema","status":"success"}`
+  ExpectValue(expectedBody, strings.Trim(w.Body.String(), "\n"), "body", t)
+
+
+  handler = http.HandlerFunc(ValidateDocument)
+  m.HandleFunc("/validate/{schemaId}", handler)
+
+  // validate valid document
+  rawDocument, err := ioutil.ReadFile("./json/test.json")
+  FailIf(err, t)
+  req, err = http.NewRequest("POST", "/validate/test-schema", bytes.NewBuffer(rawDocument))
+  FailIf(err, t)
+  w = httptest.NewRecorder()
+  m.ServeHTTP(w, req)
+  ExpectValue(http.StatusOK, w.Code, "status", t)
+  expectedBody = `{"action":"validateDocument","id":"test-schema","status":"success"}`
+  ExpectValue(expectedBody, strings.Trim(w.Body.String(), "\n"), "body", t)
+
+  // invalidate invalid document
+  rawDocument, err = ioutil.ReadFile("./json/test2.json")
+  FailIf(err, t)
+  req, err = http.NewRequest("POST", "/validate/test-schema", bytes.NewBuffer(rawDocument))
+  FailIf(err, t)
+  w = httptest.NewRecorder()
+  m.ServeHTTP(w, req)
+  ExpectValue(http.StatusBadRequest, w.Code, "status", t)
+  expectedBody = `{"action":"validateDocument","id":"test-schema","status":"error","message":"Document does not conform to schema"}`
   ExpectValue(expectedBody, strings.Trim(w.Body.String(), "\n"), "body", t)
 }
